e1e41cdb153eb2b3e337ba642fbbe624db8d0a8d
lamanda0227 <liuty0227@gmail.com>
2024-03-07 14:28:08 -0500

    simulation pipeline notebook and results added

---
+++ workflow/scripts/hrc_topmed_exome.R
@@ -0,0 +1,105 @@
+library(dplyr)
+library(tidyverse)
+library(genio)
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+from=as.numeric(args[1])
+to=as.numeric(args[2])
+rsq=as.numeric(args[4])
+
+if(args[3]=="05"){
+    maf=0.005
+} else if (args[3] == "01"){
+    maf = 0.001
+} else {
+    maf = 0.01
+}
+
+maf_c <- gsub("\\.", "", as.character(maf))
+
+exome_folder = sprintf("~/project/imputation-rvtest/workflows/imputation_aggregated_analysis/maf%s/exome", maf_c)
+hrc_folder = sprintf("~/project/imputation-rvtest/workflows/imputation_aggregated_analysis/maf%s/hrc_rsq0%d", maf_c, rsq)
+topmed_folder = sprintf("~/project/imputation-rvtest/workflows/imputation_aggregated_analysis/maf%s/topmed_rsq0%d", maf_c, rsq)
+
+annot_fname = sprintf("~/project/imputation-rvtest/analysis/imputation_aggregated_analysis/hrc_topmed_exome/hrc_topmed_exome_168206ids_rsq0%d_maf%s_annot.csv.gz", rsq, maf_c)
+
+annot <- fread(annot_fname)
+genes <- annot$Gene.refGene %>% unique()
+
+counter = from
+for(g in genes[c(from:to)]){
+    print(counter)
+    counter = counter + 1
+    
+    chr <- annot %>% filter(Gene.refGene == g) %>% pull(Chr) %>% unique()
+    fname <- paste0("chr", chr, "_", g)
+    print(fname)
+    print(dim(annot %>% filter(Gene.refGene == g)))
+
+    hrc_snplist <- annot %>% filter(Gene.refGene == g) %>% filter(source == "hrc") %>% pull(ID_hg19)
+    topmed_snplist <- annot %>% filter(Gene.refGene == g) %>% filter(source == "topmed") %>% pull(ID)
+    
+    exome_file <- list.files(exome_folder, pattern = paste0(fname,".bim"))
+    if(length(exome_file) != 0){
+        bim_fname <- paste0(exome_folder, "/", fname, '.bim')
+        fam_fname <- paste0(exome_folder, "/", fname, '.fam')
+        bed_fname <- paste0(exome_folder, "/", fname, '.bed')
+
+        bim <- read_bim(bim_fname)
+        fam <- read_fam(fam_fname)
+        bed <- read_bed(bed_fname, bim$id, fam$id)
+
+        bed_full <- bed[, order(colnames(bed))]
+        if(is.vector(bed_full)){
+            colnames <- names(as.list(bed_full))
+            bed_full <- as.data.frame(as.list(bed_full))
+            colnames(bed_full) <- colnames
+        }
+    }
+
+    hrc_fname <- paste0(hrc_folder, "/", fname, ".traw")
+    if(file.exists(hrc_fname)){
+        hrc <- fread(hrc_fname)
+        hrc_names <- colnames(hrc)[7: dim(hrc)[2]] %>% stringr::str_split(pattern = "_", simplify = TRUE)
+        colnames(hrc)[7:dim(hrc)[2]] <- hrc_names[,2]
+        
+        hrc_gene <- hrc %>% filter(SNP %in% hrc_snplist)
+        hrc_gene <- hrc_gene %>% remove_rownames() %>% select(-c("CHR", "(C)M", "POS", "COUNTED", "ALT", "SNP"))
+        hrc_gene <- 2-hrc_gene %>% select(sort(colnames(hrc_gene)))
+        
+        print("hrc snplist")
+        print(length(hrc_snplist))
+        print("hrc mat")
+        print(dim(hrc_gene))
+        if(exists("bed_full")){
+            bed_full <- rbind(bed_full, hrc_gene)
+        } else {
+            bed_full <- hrc_gene
+        }
+    }
+
+    topmed_fname <- paste0(topmed_folder, "/", fname, ".traw")
+    if(file.exists(topmed_fname)){
+        topmed <- fread(topmed_fname)
+        topmed_names <- colnames(topmed)[7: dim(topmed)[2]] %>% stringr::str_split(pattern = "_", simplify = TRUE)
+        colnames(topmed)[7:dim(topmed)[2]] <- topmed_names[,3]
+        
+        topmed_gene <- topmed %>% filter(SNP %in% topmed_snplist)
+        topmed_gene <- topmed_gene %>% remove_rownames() %>% select(-c("CHR", "(C)M", "POS", "COUNTED", "ALT", "SNP"))
+        topmed_gene <- 2 - topmed_gene %>% select(sort(colnames(topmed_gene)))
+        print("topmed snplist")
+        print(length(topmed_snplist))
+        print("topmed mat")
+        print(dim(topmed_gene))
+        if(exists("bed_full")){
+            bed_full <- rbind(bed_full, topmed_gene)
+        } else {
+            bed_full <- topmed_gene
+        }
+    }
+    fwrite(bed_full, 
+           sprintf("/home/tl3031/project/imputation-rvtest/workflows/imputation_aggregated_analysis/maf%s/hrc_topmed_exome_rsq0%d/%s.traw", 
+                   maf_c, rsq, fname))
+}
+
